44991ed6066681b909458b066bb5e3a7bc5728c6
Faraz Khan <farazkhan138@gmail.com>
2020-04-18 07:08:09 +0530

    Update mnfit2.py

--- mnfit2.py
+++ mnfit2.py
@@ -23,6 +23,7 @@ model.compile(optimizer="adam",loss="sparse_categorical_crossentropy", metrics=[
 
 model.fit(train_images,train_labels,epochs=5)
 
+# for evaluating the model,check the tested accuracy
 # test_loss,test_acc = model.evaluate(test_images,test_labels)
 # print("Tested acc:",test_acc)
 
