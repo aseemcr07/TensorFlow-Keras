242a17c426f14d9ed30164dc3673db4ba79e02f5
Faraz Khan <farazkhan138@gmail.com>
2020-04-27 17:30:17 +0530

    Update mnist-handwritten.py

--- mnist-handwritten.py
+++ mnist-handwritten.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
-# YOUR CODE SHOULD START HERE
+#callback fxn
 class StopTrainingCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if (logs.get('acc') >= 0.99):
@@ -9,25 +9,23 @@ class StopTrainingCallback(tf.keras.callbacks.Callback):
       self.model.stop_training = True
 
 mCallback = StopTrainingCallback()
-# YOUR CODE SHOULD END HERE
+
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
-# YOUR CODE SHOULD START HERE
+
 x_train = x_train / 255.0
 x_test = x_test / 255.0
-# YOUR CODE SHOULD END HERE
+
 model = tf.keras.models.Sequential([
-# YOUR CODE SHOULD START HERE
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-# YOUR CODE SHOULD END HERE
 ])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# YOUR CODE SHOULD START HERE
 model.fit(x_train, y_train, epochs=10, callbacks=[mCallback])
-# YOUR CODE SHOULD END HERE
+
+# Must add a code block to verify prediction later
